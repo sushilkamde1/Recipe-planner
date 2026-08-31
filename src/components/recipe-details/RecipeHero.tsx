acb9bb5eb7f89fa72011002cad9dfdb4e4034d9b
sushil <sushilkamde97@gmail.com>
2026-08-31 12:37:32 +0530

    update the text save button

--- src/components/recipe-details/RecipeHero.tsx
+++ src/components/recipe-details/RecipeHero.tsx
@@ -146,7 +146,7 @@ function RecipeHero({ recipe }: { recipe: Recipe }) {
               className={`rounded-full border border-yellow-100 bg-white px-4 py-2.5 text-xs font-bold text-secondary-200 transition hover:text-primary hover:border-primary ${isFavorite(recipe.id) ? "bg-primary text-primary border border-primary!" : ""}`}
               onClick={handleFavoriteClick}
             >
-              Save
+              {isFavorite(recipe.id) ? "Saved" : "Save"}
             </button>
           </div>
         </div>
